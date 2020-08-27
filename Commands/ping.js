0c1e8c9059e23c76e5faa2a1ee20dd338262b7d2
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-08-27 19:48:24 +0200

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,1 +1,6 @@
-module.exports={}
+module.exports.name="ping";
+module.exports.syntax="ping";
+module.exports.execute=(msg,args,client)=>{
+  let a = new Date();
+  msg.reply("Pong").then(sent=>sent.edit("Pong! Latency : "+msg.createdAt-new Date()+" ms."))
+}
