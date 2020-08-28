a20d065f20722509291849a205d5025739524f43
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-08-28 22:03:37 +0200

    Create invite.js

---
+++ Commands/Party/invite.js
@@ -0,0 +1,28 @@
+const {parseUser}= require("./Assets/Util.js")
+const {partyAdd,partyCreate}=require("./Assets/Party.js")
+const db=require("./Assets/db.js")
+
+module.exports={
+name:"invite",
+execute(msg,args,client,Discord){
+Promise.all(args.map(x=>parseUser(x))).then(async users=>{
+const party=await db.get("PARTYINFO"+msg.author.id);
+if(!party) await partyCreate(msg.author,msg.guild);
+if(!["owner","admin"].includes(party.role)) return msg.reply("You can't invite people to join the party you are in because you are not the owner or an admin of the party");
+if(users.every(x=>!x)) return msg.reply("All invalid users. PLease @mention them, give their ID, or type their entire username.")
+users=users.filter(x=>x)
+let partyAdds=users.each(x=>addParty(x,party.id))
+Promise.all(partyAdds).then(suc=>{
+msg.reply("Successfully added "+suc.map(x=>x.username).slice(0,4).join(", ")+(suc.length>5?" and **"+suc.length-5+"** more people":"")+" to the party! Time to party!")
+})
+
+
+})}
+
+
+
+
+
+
+
+}
