3cedc5c52ad8b313b07245dcd4b0ced671e9fe0c
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-08-29 15:54:48 +0200

    Create kick.js

---
+++ Commands/Party/kick.js
@@ -0,0 +1,25 @@
+const db=require("./Assets/db.js")
+
+module.exports={
+name:"kick",
+aliases:["remove","r","k"],
+async execute(msg,args,client,discord){
+const pinfo=await db.get("PARTYINFO"+msg.author.id)
+if(!pinfo) return msg.reply("User not in party.");
+if(!["owner","admin"].includes(pinfo.role)) return msg.reply("You can't kick people from the party you are in because you are not the owner or an admin of the party");
+if(users.every(x=>!x)) return msg.reply("All invalid users. PLease @mention them, give their ID, or type their entire username.")
+users=users.filter(x=>x)
+let partyKicks=users.each(x=>partyKick(x,party.id,msg.guild,msg.author.id))
+Promise.all(partyKicks).then(suc=>{
+msg.reply("Successfully removed "+suc.map(x=>x.username).slice(0,4).join(", ")+(suc.length>5?" and **"+suc.length-5+"** more people":"")+" from the party!")
+}).catch(e=>msg.reply("Error whilst adding users to party. Some or all of them might not be in the party. Error : "+e))
+
+
+}).catch(e=>msg.reply("Error."))
+
+}
+
+
+
+
+}
