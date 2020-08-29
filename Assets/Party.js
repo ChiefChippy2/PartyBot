945bb88c59a06b690dd9ceb98eb6e43def0ce310
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-08-29 15:24:47 +0200

    Create Party.js

---
+++ Assets/Party.js
@@ -0,0 +1,51 @@
+const db=require("./Assets/db.js")
+const perms=require("./Assets/Perms/perms.json")
+module.exports={
+async partyCreate(owner,guild){
+let tasks=[];
+
+/*then create the channel*/
+const prefs=await db.get("GUILDPREF"+guild.id)
+tasks.push(guild.channels.create(owner.displayName+"'s Party",{parent:prefs?prefs.partyChannel:msg.guild.id,type:"text",permissionOverwrites:[
+{
+id:guild.id,
+deny:["VIEW_CHANNEL"]
+},
+{
+id:owner,
+allow:["VIEW_CHANNEL","SEND_MESSAGES","MANAGE_MESSAGES","ADD_REACTIONS","USE_EXTERNAL_EMOJIS","EMBED_LINKS","ATTACH_FILES"]
+},
+]
+}))
+tasks.push(guild.channels.create(owner.displayName+"'s Party VC",{parent:prefs?prefs.partyChannel:msg.guild.id,type:"voice",permissionOverwrites:[
+{
+id:guild.id,
+deny:["VIEW_CHANNEL"]
+},
+{
+id:owner,
+allow:66061568/*View Channel,Connect,Mute Members,Move Members,Speak,Deafen Members,Use Voice Activity,Priority Speaker*/
+},
+]
+}))
+let chs=await Promise.all(tasks)
+/*register party*/
+await db.set("PARTY"+owner.id,{"guild":guild.id,"channels":chs,"id":owner.id});
+await chs[0].send(owner.toString()+", here is your party channel!")
+return chs
+
+}
+async partyAdd(user,partyid,guild){
+const party=await db.get("PARTY"+partyid)
+if(!party) throw new Error("Party Not Found.");
+/*now party is found, add user to it*/
+await Promise.all(party.channels.map(x=>guild.channels.cache.get(x)).map(x=>x.createOverwrite(user,perms.member,"Party Invite"))
+await db.set("PARTYINFO"+user.id,party)
+return user
+
+}
+
+
+
+
+}
