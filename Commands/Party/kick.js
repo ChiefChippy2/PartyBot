42b4aa253803c6b334c0c88384024f7b8c0c71a8
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-09-01 10:00:02 +0200

    Update kick.js

--- Commands/Party/kick.js
+++ Commands/Party/kick.js
@@ -11,9 +11,9 @@ if(!pinfo) return msg.reply("User not in party.");
 if(!["owner","admin"].includes(pinfo.role)) return msg.reply("You can't kick people from the party you are in because you are not the owner or an admin of the party");
 if(users.every(x=>!x)) return msg.reply("All invalid users. PLease @mention them, give their ID, or type their entire username.")
 users=users.filter(x=>x)
-let partyKicks=users.forEach(x=>partyKick(x,party.id,msg.guild,msg.author.id))
+let partyKicks=users.forEach(x=>partyKick(x,pinfo.id,msg.guild,msg.author.id))
 Promise.all(partyKicks).then(suc=>{
-if(partyEmpty(party.channels[0])) return partyDisband(party.id,msg.guild)
+if(partyEmpty(pinfo.channels[0])) return partyDisband(pinfo.id,msg.guild)
 msg.reply("Successfully removed "+suc.map(x=>x.username).slice(0,4).join(", ")+(suc.length>5?" and **"+suc.length-5+"** more people":"")+" from the party!")
 }).catch(e=>msg.reply("Error whilst adding users to party. Some or all of them might not be in the party. Error : "+e))
 
